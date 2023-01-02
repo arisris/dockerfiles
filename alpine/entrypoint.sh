6c45a67d369ed2679f678102b85b9f1430d412ae
Aris Riswanto <62107426+arisris@users.noreply.github.com>
2023-01-02 03:31:10 +0000

    DEV

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 su postgres -c 'pg_ctl start -D /var/lib/postgresql/data'
+psql -U postgres --command="ALTER USER postgres PASSWORD 'postgres'"
 # todo make it runtime
 # currently pas a "top" helper
 top
